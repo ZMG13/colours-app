22f44394d00f33f3b0cba9a8d1242f69b20943b2
zacha <20094535@mail.wit.ie>
2022-02-19 23:39:53 +0000

    Returns colours in an alphabetical order

--- src/main/kotlin/Colours.kt
+++ src/main/kotlin/Colours.kt
@@ -28,7 +28,7 @@ class Colours {
 
     fun coloursInAplhabeticOrder() : List<String>{
         //TODO return the colours in alphabetical order
-        return listOf("this should return colours in alphabetic order")
+        return colours.sorted()
     }
 
     fun coloursInAllCapitals() : List<String>{
